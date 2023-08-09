8ef9d11d599c0ceb05e9c198f6734261c158a7d3
João Lizot <premtrabajo@gmail.com>
2023-08-08 21:29:12 -0300

    alerta Div

---
+++ eventos.js
@@ -0,0 +1,13 @@
+let div = document.getElementById('div1');
+
+
+div.addEventListener('click', function() {
+   
+    alert('Hola! Soy el div');
+
+});
+
+
+function mostrarAlerta(){
+    alert('hola');
+    };
